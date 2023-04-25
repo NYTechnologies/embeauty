f19476147ffa066258926bc1b0051f4c4f95a84e
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-04-24 21:16:52 -0300

    implements register for salon and client with firebase auth

---
+++ app/src/main/java/com/nytech/embeauty/constants/ToastTextConstants.kt
@@ -0,0 +1,9 @@
+package com.nytech.embeauty.constants
+
+//mensagens de alerta para o salão e para o cliente
+
+object ToastTextConstants {
+    const val POR_FAVOR_INSIRA_SEU_EMAIL = "Por favor, insira o seu e-mail"
+    const val POR_FAVOR_INSIRA_SUA_SENHA = "Por favor, insira a sua senha"
+    const val CADASTRO_REALIZADO_COM_SUCESSO = "Cadastro realizado com sucesso!"
+}
--- app/src/main/java/com/nytech/embeauty/view/salon/SalonRegisterActivity.kt
+++ app/src/main/java/com/nytech/embeauty/view/salon/SalonRegisterActivity.kt
@@ -3,6 +3,12 @@ package com.nytech.embeauty.view.salon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.nytech.embeauty.constants.ToastTextConstants.CADASTRO_REALIZADO_COM_SUCESSO
+import com.nytech.embeauty.constants.ToastTextConstants.POR_FAVOR_INSIRA_SEU_EMAIL
+import com.nytech.embeauty.constants.ToastTextConstants.POR_FAVOR_INSIRA_SUA_SENHA
 import com.nytech.embeauty.databinding.ActivitySalonRegisterBinding
 
 /**
@@ -11,11 +17,13 @@ import com.nytech.embeauty.databinding.ActivitySalonRegisterBinding
 class SalonRegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySalonRegisterBinding
+    private lateinit var firebase: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivitySalonRegisterBinding.inflate(layoutInflater)
+        firebase = FirebaseAuth.getInstance()
 
         supportActionBar?.hide()
 
@@ -24,6 +32,57 @@ class SalonRegisterActivity : AppCompatActivity() {
         // botão para a tela de login do salão
         binding.textLoginSalon.setOnClickListener {
             startActivity(Intent(this@SalonRegisterActivity, SalonLoginActivity::class.java))
+        }
+
+        // lógica de cadastro do novo salão no Firebase
+        binding.buttonRegisterSalon.setOnClickListener {
+            val email = binding.editSalonEmail.text.toString().trim { it <= ' ' }
+            val password = binding.editSalonPassword.text.toString().trim { it <= ' ' }
+
+            //checa se o e-mail foi colocado e notifica caso não tenha sido
+            if (email.isEmpty()) {
+                Toast.makeText(
+                    this@SalonRegisterActivity,
+                    POR_FAVOR_INSIRA_SEU_EMAIL,
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            //checa se a senha foi colocada e notifica caso não tenha sido
+            if (password.isEmpty()) {
+                Toast.makeText(
+                    this@SalonRegisterActivity,
+                    POR_FAVOR_INSIRA_SUA_SENHA,
+                    Toast.LENGTH_SHORT
+                ).show()
+                return@setOnClickListener
+            }
+
+            //cria o salão no firebase auth(autenticação)
+            firebase
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+
+                        //salão cadastrado no firebase
+                        val firebaseUser: FirebaseUser = task.result!!.user!!
+
+                        //se o cadastro for completado com sucesso
+                        Toast.makeText(
+                            this@SalonRegisterActivity,
+                            CADASTRO_REALIZADO_COM_SUCESSO,
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        //se o cadastro não for completado com sucesso
+                        Toast.makeText(
+                            this@SalonRegisterActivity,
+                            task.exception!!.message.toString(),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
         }
     }
 }
